e2b1d0984c39b88a783d56a515fdbd98bab70677
Santosh Rajan <santrajan@gmail.com>
2013-04-28 22:24:31 +0530

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
 levelup-autotable
 =================
 
-Auto increment key table for node-levelup, leveldb
+Auto increment key table for node-levelup, leveldb.
+Auto incrementing keys.
+Read, write, delete fields in a record individually.
+Read, write, delete records.
+
+    db = require('levelup')('./mydb')
+    require("levelup-autotable")(db)
+
+You must call `initAutoKeys` before using the database.
+    db.initAutoKeys(callback)
+
+You can call this from inside a request handler. Initialization is done the first time round. On subsequent calls it will just call `next`.
+    app.use(function(req, res, next) {
+        db.initAutoKeys(function() {next()})
+    })
+
+Get a new key. The key is a string with integer value starting from `1`. Use `parseInt` to do math on the key. 
+    var key
+    db.newAutoKey(function(err, newKey) {
+        key = newKey
+    })
+
+Handle fields individually.
+Put a field
+    db.putField(key, "name", "John", options, callback)
+    db.putField(key, "email", "john@example.org", options, callback)
+
+The fieldname must be a valid javascript var name. ie only word chars `_` and `$` allowed.
+
+Get a field.
+    db.getField(key, fieldname, options, callback)
+
+Delete a field.
+    db.delField(key, fieldname, options, callback)
+
+Get a record. 
+    db.getRecord(key, function(err, value) {
+        console.log(value) // {name: "John", email: "john@example.org"}
+    })
+
+Put a record.
+    db.putRecord(key, {name: "John", email: "john@example.org"}, function(err) {
+        //
+    })
+
+Delete a record. 
+    db.delRecord(key, function(err) {
+        //
+    })
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,113 @@
+var counter,
+    validName = /^[a-zA-Z_$][0-9a-zA-Z_$]*$/,
+    methods = {}
+
+methods.initAutoKeys = function(callback) {
+    if (typeof counter != 'undefined') {
+        return callback()
+    }
+    this.get('__counter', function (err, value) {
+        if (err) {
+            if (err.name === 'NotFoundError') {
+                counter = 0
+            } else {
+                throw err
+            }
+        } else {
+            counter = value
+        }
+        callback()
+    })
+}
+
+methods.newAutoKey = function(callback) {
+    if (typeof callback != 'function') {
+        throw new Error("newKey requires a callback function")
+    }
+    this.put("__counter", ++counter, function(err) {
+        callback(err, counter.toString())
+    })
+}
+
+methods.putField = function(key, name, value, options, callback) {
+    if (validName.test(name)) {
+        this.put(key + ":" + name, value, options, callback)
+    } else {
+        throw new Error("Invalid char in field name")
+    }
+}
+
+methods.getField = function(key, name, options, callback) {
+    this.get(key + ":" + name, options, callback)
+}
+
+methods.delField = function(key, name, options, callback) {
+    this.del(key + ":" + name, options, callback)
+}
+
+methods.getRecord = function(key, callback) {
+    var ret = {}
+    this.createReadStream({start: key, end: parseInt(key) + 1})
+        .on('data', function (data) {
+            ret[data.key] = data.value
+        })
+        .on('error', function (err) {
+            callback(err)
+        })
+        .on('end', function (err) {
+            callback(null, ret)
+        })
+}
+
+methods.putRecord = function(key, value, callback) {
+    var ops = []
+    Object.keys(value).forEach(function(k) {
+        ops.push({type: 'put', key: key + ":" + k, value: value[k]})
+    })
+    this.batch(ops, function (err) {
+        callback(err)
+    })
+}
+
+methods.delRecord = function(key, callback) {
+    var ops = []
+    var db = this
+    this.createReadStream({start: key, end: parseInt(key) + 1})
+        .on('data', function (data) {
+            ops.push({type: 'del', key: data.key})
+        })
+        .on('error', function (err) {
+            callback(err)
+        })
+        .on('end', function (err) {
+            db.batch(ops, function (err) {
+                if (err) return callback(err)
+                callback()
+            })
+        })
+}
+
+methods.dump = function(callback) {
+    this.createReadStream()
+        .on('data', function (data) {
+            console.log(data.key, '=', data.value)
+        })
+        .on('error', function (err) {
+            if (callback) {
+                callback(err)
+            } else {
+                throw err
+            }
+        })
+        .on('end', function (err) {
+            if (callback) {
+                callback()
+            }
+        })
+}
+
+module.exports = function(db) {
+    Object.keys(methods).forEach(function(key) {
+        db[key] = methods[key]
+    })
+}
